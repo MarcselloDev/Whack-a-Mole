9465374bb0fd9cf783163d553bbd0f893503c68d
MarcselloDev <88592581+MarcselloDev@users.noreply.github.com>
2025-02-13 22:37:56 +0100

    Add score tracking

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const holes = document.querySelectorAll('.hole')
 let index = Math.floor(Math.random()*9)
 let score = 0
 let previousIndex = -1
+let moleTimer
 
 holes.forEach((hole, i) => {
   hole.addEventListener('click', () => {
     if (index === i) {
       placeMole()
       score += 1
-      console.log('Score:' + score);
+      document.getElementById('score').textContent = score
     }
   });
 });
 
 function placeMole() {
+  clearTimeout(moleTimer)
   holes[index].innerHTML= ''
   previousIndex = index
   do {
@@ -21,5 +23,15 @@ function placeMole() {
   } while (index === previousIndex)
 
   holes[index].innerHTML= '<img src="mole.png"></img>'
+  startGame()
 }
 placeMole()
+
+function startGame() {
+  moleTimer = setTimeout(placeMole, 1000)
+}
+startGame()
+
+function stopGame() {
+  clearTimeout(moleTimer)
+}
