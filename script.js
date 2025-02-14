663f52278c360153023f8e3b985a00b3832c7f1f
MarcselloDev <88592581+MarcselloDev@users.noreply.github.com>
2025-02-14 22:42:24 +0100

    Add timer to end game

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ let previousIndex = -1
 let moleTimer
 let gameStarted = false
 let highScore = localStorage.getItem('highScore') || 0
+let gameTimer = 1000
+let gameTime
 
 document.getElementById('high-score').textContent = highScore;
 
 holes.forEach((hole, i) => {
   hole.addEventListener('click', () => {
-    gameStarted = true
-    if (index === i) {
+    if (gameStarted === false) {
+      startGame()
+      gameStarted = true
+      holes[index].innerHTML= '<img src="mole.png"></img>'
+    }
+    if (index === i && gameStarted) {
       placeMole()
       score += 1
       setHighScore()
@@ -23,16 +29,21 @@ holes.forEach((hole, i) => {
 document.querySelector('.stop-btn').addEventListener('click', () => {
   stopGame()
   gameStarted = false
+  clearTimeout(moleTimer)
 })
 
 document.querySelector('.start-btn').addEventListener('click', () => {
-  clearTimeout(moleTimer)
+  stopGame()
   startGame()
   gameStarted = true
+  holes[index].innerHTML= '<img src="mole.png"></img>'
 })
 
 function placeMole() {
   clearTimeout(moleTimer)
+  if (gameStarted === true) {
+    moleTimer = setTimeout(placeMole, 1000)
+  }
   holes[index].innerHTML= ''
   previousIndex = index
   do {
@@ -41,23 +52,21 @@ function placeMole() {
 
   holes[index].innerHTML= '<img src="mole.png"></img>'
 
-  if (gameStarted === true) {
-    startGame()
-  }
-  
 }
-placeMole()
 
 function startGame() {
   moleTimer = setTimeout(placeMole, 1000)
+  startTimer()
 }
 
 function stopGame() {
-  placeMole()
   clearTimeout(moleTimer)
+  clearInterval(gameTime)
   setHighScore()
   score = 0
   document.getElementById('score').textContent = score
+  gameStarted = false
+  holes[index].innerHTML= ''
 }
 
 function setHighScore() {
@@ -66,4 +75,16 @@ function setHighScore() {
     localStorage.setItem('highScore', highScore)
     document.getElementById('high-score').textContent = highScore;
   }
+}
+
+function startTimer() {
+  gameTimer = 1000
+  gameTime = setInterval(() => {
+    gameTimer--
+    document.getElementById('timer').textContent = "Time left: " + (gameTimer/100).toFixed(2)
+    if (gameTimer === 0) {
+      stopGame()
+      clearInterval(gameTime)
+    }
+  }, 10)
 }
